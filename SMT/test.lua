cd2c7d12a8e26abad101377326b45a8107d2d150
Paojy <359661355@qq.com>
2018-09-09 23:52:56 +0800

    说明

--- SMT/test.lua
+++ SMT/test.lua
@@ -4,40 +4,40 @@ G.Dungeons[777] = {
 	{ -- 测试1
 		id = 34696,
 		alerts = {
-			AlertIcon = {
-				["1544_cast_no_164862"] = {}, -- check
-				["1544_cast_no_20473"] = {},-- check
-				["1544_com_no_19750"] = {}, -- check
+			AlertIcon = { -- 图标提示
+				["1544_cast_no_164862"] = {}, -- 地图_类型(cast施法 com对我施法 aura光环 auras多人光环 log战斗记录 bmsg首领讯息)_是否高亮（no/hl）_法术ID
+				["1544_cast_no_20473"] = {},
+				["1544_com_no_19750"] = {}, 
 			},
-			PlateAlert = {
-				[774] = "PlateAuras", -- check
-				[69607] = "PlateSpells", -- check
-				[1] = "PlatePower", -- check
+			PlateAlert = { -- 姓名板图标
+				[774] = "PlateAuras",  -- 光环
+				[69607] = "PlateSpells",  -- 技能
+				[1] = "PlatePower",  -- 能量
 			},
 			ChatMsg = {
-				[8936] = "ChatMsgAuras", -- check
-				["8936"] = "ChatMsgBossWhispers", -- check
+				[8936] = "ChatMsgAuras",  -- 受到DEBUFF喊话
+				["8936"] = "ChatMsgBossWhispers",  -- 被BOSS密语汉化
 			},
 		},
 		cd = {
-			[69607] = 1,
+			[69607] = 1, -- 姓名板技能的CD
 		},
 	},
 	{ -- 测试2
 		id = 34689,
 		alerts = {
 			AlertIcon = {
-				["1544_cast_hl_774"] = {},-- check
-				["1544_com_hl_8936"] = {},-- check
+				["1544_cast_hl_774"] = {},
+				["1544_com_hl_8936"] = {},
 			},
-			HLOnRaid = {
-				["1544_8936"] = "HL_Auras",
-				["1544_774_5"] = "HL_Cast", -- check
-				["1544_19750"] = "HL_Casting", -- check
+			HLOnRaid = { -- 团队图标高亮
+				["1544_8936"] = "HL_Auras", -- 光环
+				["1544_774_5"] = "HL_Cast",  -- 瞬发法术
+				["1544_19750"] = "HL_Casting", -- 读条法术
 			},
 			PlateAlert = {
-				[8936] = "PlateAuras", -- check
-				[69608] = "PlateSpells", -- check
+				[8936] = "PlateAuras", 
+				[69608] = "PlateSpells", 
 			},
 		},
 		cd = {
@@ -48,10 +48,10 @@ G.Dungeons[777] = {
 		id = 96444,
 		alerts = {
 			AlertIcon = {
-				["1544_aura_hl_774"] = {"HELPFUL", "player"}, -- check
-				["1544_auras_hl_774"] = {"HELPFUL"}, -- check
-				["1544_log_hl_48438"] = {"SPELL_CAST_START", nil, 3}, -- check
-				["1544_bmsg_hl_48438"] = {"CHAT_MSG_SAY", "48438", 3}, -- check
+				["1544_aura_hl_774"] = {"HELPFUL", "player"}, 
+				["1544_auras_hl_774"] = {"HELPFUL"}, 
+				["1544_log_hl_48438"] = {"SPELL_CAST_START", nil, 3}, 
+				["1544_bmsg_hl_48438"] = {"CHAT_MSG_SAY", "48438", 3}, 
 			},
 		},
 	},
